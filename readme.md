ec728c1ebf2b8c9d216c2207f075c55ee1cf14ad
Simon Weller <s.meierbeck@gmail.com>
2017-10-21 14:00:32 +0200

    address added to page and fixed height of hero image

--- readme.md
+++ readme.md
@@ -1,9 +1,9 @@
 #TODO
-- add meta tags
-
-- add JS projects (codepen)
-- add links to full pages and code
-- update Menu to have brand link on the left (and maybe collapse)
-- sketch out contact
-
 - add domain name
+- update Menu to have brand link on the left (and maybe collapse)
+- contact & impressum
+
+- full stack app examples
+- add links to full pages and code
+- move full pages to same domain
+- add meta tags
--- src/main.js
+++ src/main.js
@@ -1,9 +1,12 @@
 (() => {
+  // fade in hero-content
+  window.setTimeout(() => document.querySelector('.hero-content').classList.add('loaded'), 600)
+
+  // add transparent background to nav when scrolled
   const topnav = document.querySelector('.topnav');
   window.addEventListener('scroll', () => {
     if (window.scrollY > 0 !== topnav.classList.contains('scrolled')) {
       topnav.classList.toggle('scrolled')
     }
   })
-  window.setTimeout(() => document.querySelector('.hero-content').classList.add('loaded'), 600)
 })()
