4b2d856f8c4f146e5c1eca3efa8bbf6e7e8adb4f
Simon Weller <s.meierbeck@gmail.com>
2017-10-22 15:23:29 +0200

    tweaked top margin of hero-content to better fit mobile devices

--- src/main.js
+++ src/main.js
@@ -2,16 +2,17 @@
   // fade in hero-content
   window.setTimeout(() => document.querySelector('.hero-content').classList.add('loaded'), 600)
 
-  // add transparent background to nav when scrolled
   const topnav = document.querySelector('.topnav');
+  const menuButton = document.querySelector('button.menu');
+
+  // add transparent background to nav when scrolled
   window.addEventListener('scroll', () => {
     if (window.scrollY > 0 !== topnav.classList.contains('scrolled')) {
       topnav.classList.toggle('scrolled')
     }
   })
 
-  // toggle open class on menu button
-  const menuButton = document.querySelector('button.menu');
+  // toggle open class of .topnav on click of menu button
   menuButton.addEventListener('click', () => {
     topnav.classList.toggle('open')
   })
